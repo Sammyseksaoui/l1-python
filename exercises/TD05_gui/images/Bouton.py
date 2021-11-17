2930287a4c714cc5704b500f6bb6ae9f8fa5154b
Sammyseksaoui <91132836+Sammyseksaoui@users.noreply.github.com>
2021-11-17 18:46:02 +0100

    Modifications Bouton

---
+++ exercises/TD05_gui/images/Bouton.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+    
+
+def write_slogan():
+    print("Trump is great!")
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+canvas1 = tk.Canvas(root, relief = tk.FLAT, background = "black")
+canvas1.pack()
+
+button = tk.Button(frame, 
+                   text="QUIT", 
+                   fg="red",
+                   command=quit)
+button.pack(side=tk.LEFT)
+slogan = tk.Button(frame,
+                   text="Trump",
+                   command=write_slogan)
+slogan.pack(side=tk.LEFT)
+
+root.mainloop()
